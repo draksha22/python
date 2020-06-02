dcbb5a21ac39ce0a5b4db3adb94b4c725fcc435d
Draksha22 <drakshakhan2211@gmail.com>
2020-06-02 16:00:21 +0530

    it tells if a no. is even or odd

---
+++ even-odd.py
@@ -0,0 +1,14 @@
+x = int(input("Enter the number you want to check"))
+
+if x%2==0:
+    print("number is even")
+
+if x%2!=0:
+    print("number is odd")
+
+y = int(input("Enter the number you want to check"))
+if y%2==0:
+    print("Even")
+
+else:
+    print("Odd")

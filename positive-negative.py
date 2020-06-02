722805ffaaba1a05acf294c15183d35febf99478
Draksha22 <drakshakhan2211@gmail.com>
2020-06-02 16:02:15 +0530

    it tells whether a given no is positive or negative

---
+++ positive-negative.py
@@ -0,0 +1,15 @@
+x = int(input("Enter a number"))
+
+if x > 0 :
+    print("Positive")
+
+elif x < 0:
+    print("Negative")
+
+y = int(input("Enter a number"))
+
+if y > 0 :
+    print("Positive")
+
+else:
+    print("Negative")

91549844497a19ea9e1d2e731b52e2bf0b1f411d
Draksha22 <drakshakhan2211@gmail.com>
2020-06-03 16:25:39 +0530

    how to find perfect sqaure of a number

---
+++ perfect-square.py
@@ -0,0 +1,12 @@
+for i in range(1,500):
+    if i**2 <= 500:
+        print(i**2)
+    else:
+        break
+
+
+import  math
+for i in range(1,500):
+    a=math.sqrt(i)
+    if i%a==0:
+        print(i)

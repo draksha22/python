ed87548db5fef765b4440d0c7e0af865aaf83c88
Draksha22 <drakshakhan2211@gmail.com>
2020-06-07 21:16:00 +0530

    how to perform different operations in an array

---
+++ operations-array.py
@@ -0,0 +1,42 @@
+from numpy import *
+arr = array([1,2,3,4,5])
+arr = arr + 5
+print(arr)
+
+from numpy import *
+arr1 = array([1,2,3,4,5])
+arr2 = array([7,2,5,3,1])
+arr = arr1 + arr2
+print(arr)
+
+from numpy import *
+arr = array([1,2,3,4,5])
+print(cos(arr))
+
+from numpy import *
+arr = array([1,2,3,4,5])
+print(log(arr))
+
+from numpy import *
+arr = array([1,2,3,4,5])
+print(sqrt(arr))
+
+from numpy import *
+arr = array([1,2,3,4,5])
+print(sum(arr))
+
+from numpy import *
+arr = array([1,2,3,4,5])
+print(min(arr))
+
+from numpy import *
+arr = array([1,2,3,4,5])
+print(max(arr))
+
+from numpy import *
+arr1 = array([1,2,3,4,5])
+arr2 = array([2,4,1,6,4])
+print(concatenate([arr1,arr2]))
+
+
+

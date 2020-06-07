d6aed7242833a69cc1f38bf64acfbdacddf40454
Draksha22 <drakshakhan2211@gmail.com>
2020-06-07 21:11:16 +0530

    how to create an array using different methods

---
+++ creating-array.py
@@ -0,0 +1,25 @@
+from numpy import *
+from numpy.ma import arange, ones, zeros
+
+arr = array([1,2,3,4,5])
+print(arr.dtype)
+
+from numpy import *
+arr = linspace(0,16,10)
+print(arr)
+
+from numpy import *
+arr = arange(1,15,2)
+print(arr)
+
+from numpy import *
+arr = logspace(1,40,5)
+print(arr)
+
+from numpy import *
+arr = zeros(5)
+print(arr)
+
+from numpy import *
+arr = ones(5)
+print(arr)

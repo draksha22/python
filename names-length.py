38cbc0d60832459fca08d56e3ffb8c2986824d34
Draksha22 <drakshakhan2211@gmail.com>
2020-06-09 21:32:53 +0530

    count and display the names of users who has name length greater than 5 letters

---
+++ names-length.py
@@ -0,0 +1,7 @@
+def name(list):
+    for i in list:
+        if len(i)>5:
+            print(i)
+
+list = ['emily','chandler','misha','rachel','joey','monica','riya','ross','janice','iram']
+name(list)
